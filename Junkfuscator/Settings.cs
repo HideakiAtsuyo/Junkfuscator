8a86b11c648fe009872622d4ea3b2606f39e0fa6
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2022-01-08 17:24:17 -0800

    Update Settings

--- Junkfuscator/Settings.cs
+++ Junkfuscator/Settings.cs
@@ -7,7 +7,7 @@ namespace Junkfuscator
     {
         public static string WM = "github.com/HideakiAtsuyo", WMB64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(WM));
 
-        public static int AntiDe4DotInterfaces = 1000, Junks = 50000, junksMethods = 5, nopPerMethod = 250;
+        public static int AntiDe4DotInterfaces = 2000, Junks = 51000, junksMethods = 15, nopPerMethod = 3;
         public static string[] no = { "ZG5zcHk=", WM, WMB64 };
     }
 }
